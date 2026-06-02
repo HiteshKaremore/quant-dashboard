1d66c0ca83a9337098a131cc16e369ad5ee3980d
Hitesh Karemore <hiteshkaremore@Hiteshs-MacBook-Air.local>
2026-06-02 12:48:15 +0530

    added components to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,24 @@ import Sidebar from "./components/Sidebar"
 import Navbar from "./components/Navbar"
 import DashboardCard from "./components/DashboardCard"
 
+const dashboardData = [
+  {
+    title: "Portfolio Value",
+    value: "$124,500",
+  },
+
+  {
+    title: "Today's Profit",
+    value: "+$2,430",
+    color: "text-green-400",
+  },
+
+  {
+    title: "Active Positions",
+    value: "12",
+  },
+]
+
 function App() {
   return (
     <div className="flex bg-[#0f172a] text-white min-h-screen">
@@ -14,21 +32,16 @@ function App() {
 
         <div className="grid grid-cols-3 gap-6">
 
-          <DashboardCard
-            title="Portfolio Value"
-            value="$124,500"
-          />
-
-          <DashboardCard
-            title="Today's Profit"
-            value="+$2,430"
-            color="text-green-400"
-          />
-
-          <DashboardCard
-            title="Active Positions"
-            value="12"
-          />
+          {
+  dashboardData.map((item, index) => (
+    <DashboardCard
+      key={index}
+      title={item.title}
+      value={item.value}
+      color={item.color}
+    />
+  ))
+}
 
         </div>
 
